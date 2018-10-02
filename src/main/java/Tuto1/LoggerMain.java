25017f59abfe7785097e637a24977dcee24f048a
SchMatthieu <43790124+SchMatthieu@users.noreply.github.com>
2018-10-02 18:40:35 +0200

    Add files via upload

---
+++ src/main/java/Tuto1/LoggerMain.java
@@ -0,0 +1,10 @@
+package Tuto1;
+
+import org.apache.log4j.Logger;
+
+public class LoggerMain {
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+    public static void main(String[] args) {
+        LOGGER.error("Hello World !");
+    }
+}
